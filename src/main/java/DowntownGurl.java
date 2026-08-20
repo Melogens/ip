b4c54d30982c772ccb7727ff3cbd7147b204e011
Melogens <melodyphyusin@gmail.com>
2026-08-20 17:12:55 +0800

    Add echo

--- src/main/java/DowntownGurl.java
+++ src/main/java/DowntownGurl.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 /**
  * Entry point for the Downtown Gurl chatbot application.
  */
 public class DowntownGurl {
     private static final String CHATBOT_NAME = "Downtown Gurl";
-    private static final String DIVIDER = "____________________________________________________________";
+    private static final String DIVIDER = "______________________________________________________________________";
 
     public static void main(String[] args) {
         String banner = """
@@ -17,8 +19,19 @@ public class DowntownGurl {
         System.out.println(DIVIDER);
         System.out.println("Hey I'm " + CHATBOT_NAME + ".");
         System.out.println("I'm the one to give you a reality check and help you manifest that life you've been dreaming.");
+        System.out.println("Darling what's up?");
         System.out.println(DIVIDER);
-        System.out.println("That's bombz. Byes!");
-        System.out.println(DIVIDER);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println("That's bombz. Byes!");
+                System.out.println(DIVIDER);
+                break;
+            }
+            System.out.println(command);
+            System.out.println(DIVIDER);
+        }
     }
 }
