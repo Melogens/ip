03c84687d67e98c25ab45130b4acc3db72703fa4
Melogens <melodyphyusin@gmail.com>
2026-08-20 15:42:23 +0800

    Add greeting and goodbye

---
+++ src/main/java/DowntownGurl.java
@@ -0,0 +1,24 @@
+/**
+ * Entry point for the Downtown Gurl chatbot application.
+ */
+public class DowntownGurl {
+    private static final String CHATBOT_NAME = "Downtown Gurl";
+    private static final String DIVIDER = "____________________________________________________________";
+
+    public static void main(String[] args) {
+        String banner = """
+                 ____                      _                       ____           __\s
+                |  _ \\  _____      ___ __ | |_ _____      ___ __  / ___|_   _ _ __| |
+                | | | |/ _ \\ \\ /\\ / / '_ \\| __/ _ \\ \\ /\\ / / '_ \\| |___| | | '__| | |
+                | |_| | (_) \\ V  V /| | | | || (_) \\ V  V /| | | | |_| | |_| | |  | |
+                |____/ \\___/ \\_/\\_/ |_| |_|\\__\\___/ \\_/\\_/ |_| |_|\\____|\\__,_|_|  |_|""";
+        System.out.println(DIVIDER);
+        System.out.println(banner);
+        System.out.println(DIVIDER);
+        System.out.println("Hey I'm " + CHATBOT_NAME + ".");
+        System.out.println("I'm the one to give you a reality check and help you manifest that life you've been dreaming.");
+        System.out.println(DIVIDER);
+        System.out.println("That's bombz. Byes!");
+        System.out.println(DIVIDER);
+    }
+}
